5dbf064f96d59f02938dae20ca5ccf9e6c2402b8
= <=>
2022-05-20 15:02:22 -0400

    Se customiza el widget button

--- lib/src/pages/zapato_desc_page.dart
+++ lib/src/pages/zapato_desc_page.dart
@@ -15,17 +15,47 @@ class ZapatoDescPage extends StatelessWidget {
         Expanded(
           child: SingleChildScrollView(
             child: Column(
-              children: const [
-                ZapatosDescripcionPage(
+              children: [
+                const ZapatosDescripcionPage(
                   titulo: 'Nike Air Max 720',
                   descripcion:
                       "The Nike Air Max 720 goes bigger than ever before with Nike's taller Air unit yet, offering more air underfoot for unimaginable, all-day comfort. Has Air Max gone too far? We hope so.",
-                )
+                ),
+                _MontoBuyNow(),
               ],
             ),
           ),
         ),
       ],
     ));
+  }
+}
+
+class _MontoBuyNow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Container(
+        margin: const EdgeInsets.only(top: 30),
+        child: Row(
+          children: const [
+            Text(
+              '\$180.0',
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Spacer(),
+            BotonNaranja(
+              texto: 'Buy Now',
+              ancho: 120,
+              alto: 35,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/src/widgets/boton_naranja.dart
+++ lib/src/widgets/boton_naranja.dart
@@ -2,15 +2,17 @@ import 'package:flutter/material.dart';
 
 class BotonNaranja extends StatelessWidget {
   final String texto;
+  final double alto;
+  final double ancho;
 
-  const BotonNaranja({this.texto});
+  const BotonNaranja({this.texto, this.alto = 50, this.ancho = 150});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.center,
-      width: 150,
-      height: 50,
+      width: ancho,
+      height: alto,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(100),
         color: Colors.orange,
