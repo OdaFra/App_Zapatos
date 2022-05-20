237141380313b95a9bf6a14a8b81c594d4bcc497
= <=>
2022-05-20 16:00:04 -0400

    FloatingActionButton para navegación de retroceso

--- lib/src/pages/zapato_desc_page.dart
+++ lib/src/pages/zapato_desc_page.dart
@@ -1,4 +1,3 @@
-import 'package:app_zapatos/src/widgets/zapato_size.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/custom_widgets.dart';
@@ -9,8 +8,25 @@ class ZapatoDescPage extends StatelessWidget {
     return Scaffold(
         body: Column(
       children: [
-        const ZapatoSizePage(
-          fullScreen: true,
+        Stack(
+          children: [
+            const ZapatoSizePage(fullScreen: true),
+            Positioned(
+                top: 50,
+                left: 15,
+                child: FloatingActionButton(
+                  backgroundColor: Colors.transparent,
+                  onPressed: () {},
+                  // ignore: sort_child_properties_last
+                  child: const Icon(
+                    Icons.arrow_back_ios,
+                    color: Colors.white,
+                    size: 35,
+                  ),
+                  elevation: 0,
+                  highlightElevation: 0,
+                ))
+          ],
         ),
         Expanded(
           child: SingleChildScrollView(
@@ -23,13 +39,60 @@ class ZapatoDescPage extends StatelessWidget {
                       "The Nike Air Max 720 goes bigger than ever before with Nike's taller Air unit yet, offering more air underfoot for unimaginable, all-day comfort. Has Air Max gone too far? We hope so.",
                 ),
                 _MontoBuyNow(),
-                _ColoresYmas()
+                _ColoresYmas(),
+                _BotonesLikesCartSettings()
               ],
             ),
           ),
         ),
       ],
     ));
+  }
+}
+
+class _BotonesLikesCartSettings extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 30),
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          const _BotonSombreado(
+              Icon(Icons.favorite, color: Colors.red, size: 25)),
+          _BotonSombreado(Icon(Icons.add_shopping_cart,
+              color: Colors.grey.withOpacity(0.8), size: 25)),
+          _BotonSombreado(Icon(Icons.settings,
+              color: Colors.grey.withOpacity(0.8), size: 25)),
+        ],
+      ),
+    );
+  }
+}
+
+class _BotonSombreado extends StatelessWidget {
+  final Icon icon;
+
+  const _BotonSombreado(this.icon);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 55,
+      height: 55,
+      decoration: const BoxDecoration(
+          color: Colors.white,
+          shape: BoxShape.circle,
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black12,
+                spreadRadius: -5,
+                blurRadius: 20,
+                offset: Offset(0, 10))
+          ]),
+      child: icon,
+    );
   }
 }
 
